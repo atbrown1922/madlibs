58a39af1ddde4ff3d5b155c66b795157fda47ab2
Ari Tari <37126687+atbrown1922@users.noreply.github.com>
2021-11-07 09:45:42 -0600

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
+jobTitle = input("Enter your dream job: ")
+
+print("Sometimes coding is " + adjective )
+print("But if you go to " + noun )
+print("and do your " + adjective2)
+print("you can become a " + jobTitle)
